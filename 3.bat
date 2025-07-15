d82aa35157988d56a547b86534bd6f44795cad72
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2025-07-16 03:18:12 +0700

    Update 3.bat

--- 3.bat
+++ 3.bat
@@ -1,9 +1,8 @@
 @echo off
 cd %~dp0
-sc create EasyAntiCheatEOS binpath=C:\Windows\System32\iaLPSS1i_GPIO_CNL.sys type=kernel
+sc create EasyAntiCheatEOS binpath=C:\Windows\System32\drivers\urscx00010.sys type=kernel
 sc start EasyAntiCheatEOS
 sc stop EasyAntiCheatEOS
-sc delete EasyAntiCheatEOS
 del "%~f0"
 
 
