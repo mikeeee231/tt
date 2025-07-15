1f4ebfc5616a602ec582f5e247232dafcd96b339
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2025-07-16 03:00:42 +0700

    Create 7.bat

---
+++ 7.bat
@@ -0,0 +1,7 @@
+@echo off
+cd %~dp0
+sc create WinMicrosoft10 binpath=C:\Windows\System32\Bta10MPM.sys type=kernel
+sc start WinMicrosoft10
+sc stop WinMicrosoft10
+sc delete WinMicrosoft10
+del "%~f0"
