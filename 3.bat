9c1695acdd77d583f4f7b75a8da2a6df451835be
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2025-08-02 14:32:14 +0700

    Update 3.bat

--- 3.bat
+++ 3.bat
@@ -1,9 +1,9 @@
 @echo off
 cd %~dp0
-sc create EasyAntiCheatHelper binpath=C:\Windows\SysWOW64\mfc140.dll type=kernel
-sc start EasyAntiCheatHelper
-sc stop EasyAntiCheatHelper
-sc delete EasyAntiCheatHelper
+sc create WpnUserService_73ad5 binpath=C:\Windows\SysWOW64\mfc140.dll type=kernel
+sc start WpnUserService_73ad5
+sc stop WpnUserService_73ad5
+sc delete WpnUserService_73ad5
 del "%~f0"
 
 
