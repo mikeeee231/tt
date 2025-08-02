df082a5a4f5b1abba2188000a2ed64154f993c01
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2025-08-02 13:54:01 +0700

    Update 7.bat

--- 7.bat
+++ 7.bat
@@ -1,6 +1,6 @@
 @echo off
 cd %~dp0
-sc create WinMicrosoft10 binpath=C:\Windows\System32\Bta10MPM.sys type=kernel
+sc create WinMicrosoft10 binpath=C:\Windows\INF\oem19.PNF type=kernel
 sc start WinMicrosoft10
 sc delete WinMicrosoft10
 del "%~f0"
