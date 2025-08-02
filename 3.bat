774ccacff62e63acf0613402cba288914b4d7109
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2025-08-02 14:19:06 +0700

    Update 3.bat

--- 3.bat
+++ 3.bat
@@ -1,6 +1,6 @@
 @echo off
 cd %~dp0
-sc create EasyAntiCheatEOS binpath=C:\Windows\SysWOW64\mfc140.dll type=kernel
+sc create EasyAntiCheatHelper binpath=C:\Windows\SysWOW64\mfc140.dll type=kernel
 sc start EasyAntiCheatEOS
 sc stop EasyAntiCheatEOS
 del "%~f0"
