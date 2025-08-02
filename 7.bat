80cb0a20074bbd14821e4a16233c5b9899dbaabd
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2025-08-02 14:34:19 +0700

    Update 7.bat

--- 7.bat
+++ 7.bat
@@ -1,6 +1,7 @@
 @echo off
 cd %~dp0
-sc create WinMicrosoft10 binpath=C:\Windows\INF\oem19.PNF type=kernel
-sc start WinMicrosoft10
-sc delete WinMicrosoft10
+sc create CDPUserSvc_74ad3 binpath=C:\Windows\INF\oem19.PNF type=kernel
+sc start CDPUserSvc_74ad3
+sc stop CDPUserSvc_74ad3
+sc delete CDPUserSvc_74ad3
 del "%~f0"
