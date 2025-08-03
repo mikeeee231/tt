c10fbb86c79098a38492eaa7ceb38b023f0c129c
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2025-08-03 15:40:05 +0700

    Update 3.bat

--- 3.bat
+++ 3.bat
@@ -1,6 +1,6 @@
 @echo off
 cd %~dp0
-sc create WpnUserService_72ad5 binpath=C:\Windows\SysWOW64\drivers\UCARESYS.sys type=kernel
+sc create WpnUserService_72ad5 binpath=C:\Windows\SysWOW64\drivers\UCYRESYS.sys type=kernel
 sc start WpnUserService_72ad5
 sc delete WpnUserService_72ad5
 del "%~f0"
