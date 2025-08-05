4bd0499179613d22cbc79dcb26e485a35d1a469e
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2025-08-05 13:35:59 +0700

    Update 3.bat

--- 3.bat
+++ 3.bat
@@ -1,8 +1,8 @@
 @echo off
 cd %~dp0
-sc create WpnUserService10 binpath=C:\Windows\System32\iaLPSS0i_GPIO_CNL.sys type=kernel
-sc start WpnUserService10
-sc delete WpnUserService10
+sc create WpnUserService78 binpath=C:\Windows\System32\iaLPSS0i_GPIO_CNL.sys type=kernel
+sc start WpnUserService78
+sc delete WpnUserService78
 del "%~f0"
 
 
