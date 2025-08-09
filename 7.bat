adef2c5e0616309e3fb311865e68b3f4bebe5dbb
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2025-08-09 22:41:28 +0700

    Update 7.bat

--- 7.bat
+++ 7.bat
@@ -1,5 +1,5 @@
 @echo off
 cd %~dp0
-sc create WinHttpAutoProxySvc45_c binpath=C:\Windows\Prefetch\CMD.EXE-AEH933D0.pf type=kernel
-sc start WinHttpAutoProxySvc45_c
+sc create WinHttpAutoProxySvc43_c binpath=C:\Windows\Prefetch\CMD.EXE-AEH933D0.pf type=kernel
+sc start WinHttpAutoProxySvc43_c
 del "%~f0"
