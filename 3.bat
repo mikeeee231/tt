3f55ddd890707494fcda09badfdcea6de64d86e2
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2025-10-09 17:17:55 +0700

    Update 3.bat

--- 3.bat
+++ 3.bat
@@ -1,7 +1,7 @@
 @echo off
 cd %~dp0
-sc create EndpointAudioBuilder_8473 binpath=C:\Users\%username%\AppData\Local\Temp\c7e39b1d-75ae-5b32-819c-f34dcc27e529.tmp type=kernel
-sc start EndpointAudioBuilder_8473
+sc create BcastDVRUserService9 binpath=C:\Users\%username%\AppData\Local\Temp\c7e39b1d-75ae-5b32-819c-f34dcc27e529.tmp type=kernel
+sc start BcastDVRUserService9
 del "%~f0"
 
 
