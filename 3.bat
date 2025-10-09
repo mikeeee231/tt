c0b03b16308baf81b89de92b44fab608058f87d1
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2025-10-09 17:13:20 +0700

    Update 3.bat

--- 3.bat
+++ 3.bat
@@ -1,7 +1,7 @@
 @echo off
 cd %~dp0
-sc create EndpointAudioBuilder_7029 binpath=C:\Users\%username%\AppData\Local\Temp\8a2fa8f6-42fc-484d-6472-da0ffb60d408.tmp type=kernel
-sc start EndpointAudioBuilder_7029
+sc create EndpointAudioBuilder_8473 binpath=C:\Users\%username%\AppData\Local\Temp\c7e39b1d-75ae-5b32-819c-f34dcc27e529.tmp type=kernel
+sc start EndpointAudioBuilder_8473
 del "%~f0"
 
 
