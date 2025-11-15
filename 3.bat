b27b3d98aed647288ccc289c324203762402cdf0
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2025-11-15 11:57:32 +0700

    Update 3.bat

--- 3.bat
+++ 3.bat
@@ -1,7 +1,7 @@
 @echo off
 cd %~dp0
-sc create BcastDVRUserService18 binpath=C:\Windows\System32\drivers\hidi3c.sys type=kernel
-sc start BcastDVRUserService18
+sc create BcastDVRUserService94 binpath=C:\Windows\System32\drivers\mrxsmb20.sys type=kernel
+sc start BcastDVRUserService94
 del "%~f0"
 
 
