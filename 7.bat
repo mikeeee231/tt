1d282bd4f995402c61ff8e45bbe79e2e89393638
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2025-12-12 00:00:03 +0700

    Update 7.bat

--- 7.bat
+++ 7.bat
@@ -1,5 +1,5 @@
 @echo off
 cd %~dp0
-sc create BluetoothUserService74 binpath=C:\Windows\System32\drivers\cht4vx32.sys type=kernel
-sc start BluetoothUserService74
+sc create BluetoothUserService5964 binpath=C:\Windows\System32\drivers\SerCx3.sys type=kernel
+sc start BluetoothUserService5964
 del "%~f0"
