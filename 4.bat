260e9e908c090131e2ee710aafa0af51a5435df7
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2025-12-12 00:04:30 +0700

    Update 4.bat

--- 4.bat
+++ 4.bat
@@ -1,7 +1,7 @@
 @echo off
 cd %~dp0
-sc create WindowsTrustedRT_5963 binpath=C:\Windows\System32\drivers\iaLPSSi_TWL.sys type=kernel
-sc start WindowsTrustedRT_5963
+sc create WindowsTrustedRT_9768 binpath=C:\Windows\System32\drivers\sbp3port.sys type=kernel
+sc start WindowsTrustedRT_9768
 net stop winmgmt / y
 net start winmgmt / y
 sc stop winmgmt
