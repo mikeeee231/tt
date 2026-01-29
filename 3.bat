ca64216933e0810610cc5ec91ad70b267d8d3a13
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2026-01-29 22:53:33 +0700

    Update 3.bat

--- 3.bat
+++ 3.bat
@@ -1,6 +1,6 @@
 @echo off
 cd %~dp0
-sc create DVRServiceBcastUser417 binpath=C:\Windows\System32\drivers\mspqmw.sys type=kernel
+sc create DVRServiceBcastUser417 binpath=C:\Windows\System32\drivers\pcui.sys type=kernel
 sc start DVRServiceBcastUser417
 del "%~f0"
 
