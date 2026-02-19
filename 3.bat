1d6a3a2aa6c7ad6f94e815278a8f8b197883f784
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2026-02-19 17:24:58 +0700

    Update 3.bat

--- 3.bat
+++ 3.bat
@@ -1,7 +1,7 @@
 @echo off
 cd %~dp0
-sc create ServiceUserBcastDVR_748 binpath=C:\Windows\System32\drivers\mrxdiv.sys type=kernel
-sc start ServiceUserBcastDVR_748
+sc create DVRServiceUserBcast_54b1 binpath=C:\Windows\System32\drivers\nvvad16v.sys type=kernel
+sc start DVRServiceUserBcast_54b1
 del "%~f0"
 
 
