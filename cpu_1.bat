bc944465661f742995e81d80e52479e900f2f408
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2026-02-24 16:02:36 +0700

    Update cpu_1.bat

--- cpu_1.bat
+++ cpu_1.bat
@@ -5,9 +5,9 @@ pushd "%CD%"
 cd C:\WINDOWS\ServiceProfiles\LocalService\1
 
 
-AMIDEWINx64 /PSN "Unknown"
-AMIDEWINx64 /PAT "Unknown"
-AMIDEWINx64 /PPN "Unknown"
+AMIDEWINx64 /PSN "To Be Filled By O.E.M."
+AMIDEWINx64 /PAT "To Be Filled By O.E.M."
+AMIDEWINx64 /PPN "To Be Filled By O.E.M."
 
 
 
