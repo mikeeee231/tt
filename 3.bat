7fce371429182058a27c2e81b1f7980d6c4a42d9
842834fdjfgfg <109022461+TrinityCheat@users.noreply.github.com>
2026-03-16 15:51:33 +0700

    Update 3.bat

--- 3.bat
+++ 3.bat
@@ -1,7 +1,7 @@
 @echo off
 cd %~dp0
-sc create ServiceUserDVRBcast_42f8 binpath=C:\Windows\System32\drivers\RfxVmt16v.sys type=kernel
-sc start ServiceUserDVRBcast_42f8
+sc create ServiceDVRUserBcast_32f1 binpath=C:\Windows\System32\drivers\RfxVmt16.sys type=kernel
+sc start ServiceDVRUserBcast_32f1
 del "%~f0"
 
 
